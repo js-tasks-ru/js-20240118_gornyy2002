31f7b4c48a3497398c32e30134d11d86c54cfd8e
Vladimir <gornyy2002@mail.ru>
2024-01-14 13:09:03 +0300

    solution for dz2 in mod3

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,12 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+    if(obj == undefined){
+        return undefined;
+    }
+    let new_obj = {};
+    for(let key in obj){
+        new_obj[obj[key]] = key;
+    }
+    return new_obj;
 }
