ba786ac7f6973f54e651f604860e1c73bf78e2ff
Vladimir <gornyy2002@mail.ru>
2024-01-14 13:00:46 +0300

    solution for dz1 in mod3

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,8 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    let props = path.split('.');
+    return function(obj) {
+        return props.reduce((acc, prop) => acc?.[prop], obj);
+      };
 }
