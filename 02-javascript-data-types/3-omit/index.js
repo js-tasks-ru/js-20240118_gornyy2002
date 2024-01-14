5d50258963d779c2d087397014871c7b961dba1e
Vladimir <gornyy2002@mail.ru>
2024-01-14 12:39:43 +0300

    add solution for dz3 in module 1

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,12 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+    let entrie = Object.entries(obj);
+    let new_obj = {};
+    for(ar of entrie){
+       if(!fields.includes(ar[0])){
+           new_obj[ar[0]] = ar[1];
+       }
+    }
+    return new_obj;
 };
