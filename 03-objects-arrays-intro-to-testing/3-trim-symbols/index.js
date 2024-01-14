9d69d40372d0a54fec3af93ea58a207178e6a2b7
Vladimir <gornyy2002@mail.ru>
2024-01-14 13:54:26 +0300

    solution for dz3 in mod 3

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,29 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+    let arr = string.split('');
+    let len = arr.length;
+    let lett = arr[0];
+    let i = 1;
+    let numEq = 1;
+    if(size == 0 ){
+        return '';
+    }
+    while( i  < len){
+        if(arr[i] == lett){
+            numEq+=1;
+            if(numEq > size){
+                arr.splice(i, 1);
+                len =arr.length;
+                numEq-=1;
+                i-=1;
+            }
+        } else{
+            numEq = 1;
+            lett = arr[i];
+        }
+        i+=1;
+    }
+    
+    return arr.join('');
 }
