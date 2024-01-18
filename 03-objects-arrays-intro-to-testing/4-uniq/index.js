aaf49995601ec7fd9bb14ffc3be3661d05174089
Vladimir Gornyy <112384479+The0d4mar@users.noreply.github.com>
2024-01-18 17:49:26 +0300

    solution for dz4(uniq) in mod3

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,24 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-
+    if(arr == undefined || arr.length  == 0 ){
+        return new Array();
+    }
+    let arr_d = [...arr];
+    let ar_obj = {};
+    arr.forEach(function (x) { ar_obj[x] = (ar_obj[x] || 0) + 1; });
+    let it_ar = Array.from(Object.keys(ar_obj));
+    for(i in it_ar){
+        if(isNaN(Number(it_ar[i]))){
+            continue;
+        } else{
+            it_ar[i] = Number(it_ar[i]);
+        }
+    }
+    it_ar.sort((a,b)=>{
+        if(arr.indexOf(a) > arr.indexOf(b)){
+            return 1;
+        } else{return -1;}
+    });
+    return it_ar;
 }
