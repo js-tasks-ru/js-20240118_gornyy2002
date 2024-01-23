086111b24bb082edf7f9b6e394c9102fd82bf7cc
Vladimir Gornyy <112384479+The0d4mar@users.noreply.github.com>
2024-01-23 12:30:01 +0300

    columnchart solution in mod 4

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,70 @@
 export default class ColumnChart {
+    element;
+    data;
+    label;
+    value;
+    link;
+    chartHeight = 50;
+  
+    constructor(param = {}) {
+        this.data = param.data || [];
+        this.label = param.label || '';
+        this.value = param.value || '';
+        this.link = param.link || '';
+        this.render();
+      }
 
+        getDataHTML() {
+            const maxValue = Math.max(...this.data);
+            const scale = this.chartHeight / maxValue;
+            return this.data.reduce(function (dataHTML, value) {
+                return dataHTML +  `<div style="--value:${Math.floor(value * scale)}"
+                data-tooltip="${(value / maxValue * 100).toFixed(0)}%">
+        </div>`
+            }, '');
+        }
+
+        getLinkHTML() {
+            return this.link ? `<a class="column-chart__link" href="${this.link}">Посмотреть все</a>` : '';
+        }
+
+        render() {
+            const div = document.createElement('div');
+
+            div.className = "column-chart";
+            if (!this.data.length) {
+            div.className += " column-chart_loading";
+            }
+            if(this.label == 'sales'){
+                div.innerHTML = `<div class="column-chart__title">Total ${this.label}${this.getLinkHTML()}</div>
+                <div class="column-chart__container">
+                    <div class="column-chart__header">$${this.value}</div>
+                    <div class="column-chart__chart">
+                        ${this.getDataHTML()}
+                    </div>
+                </div>`;
+            }
+            else{
+                div.innerHTML = `<div class="column-chart__title">Total ${this.label}${this.getLinkHTML()}</div>
+                <div class="column-chart__container">
+                    <div class="column-chart__header">${this.value}</div>
+                    <div class="column-chart__chart">
+                        ${this.getDataHTML()}
+                    </div>
+                </div>`;
+            }
+
+            
+            this.element = div;
+        }
+
+        update(data) {
+            if (this.data.length && (data || !data.length)) {
+                this.element.className += " column-chart_loading";
+            }
+
+            this.data = data || [];
+            const dataElement = this.element.querySelector(".column-chart__chart");
+            dataElement.innerHTML = this.getDataHTML();
+        }
 }
